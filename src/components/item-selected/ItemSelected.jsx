d15907a3f0156c53d44e3d5936bf454fcd635c51
Nurbolot <nubolotkadyrov06@gmail.com>
2024-04-16 04:13:01 +0600

    create new component name:ItemSelected

--- src/components/item-selected/ItemSelected.jsx
+++ src/components/item-selected/ItemSelected.jsx
@@ -1,12 +1,42 @@
+import DescAbout from '../desc-about-food/DescAbout'
 import itemClass from './ItemSelected.module.scss'
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
+import Amount from '../amount/Amount';
 
-const ItemSelected = ()=>{
-      
+const ItemSelected = ({selected})=>{
+      const [openIsTheSelectedClick,setIsOpenTheSelectedClick]=useState(false)
+      const funcOpen = ()=>{
+            setIsOpenTheSelectedClick(open=>!open)
+      }
 
       return(
-            <div className={}>
-
+            <div className={itemClass.content}>
+            <div className={itemClass.line}>
+            </div>                  
+            <div className={itemClass.contentTwo}>
+                  <div className={itemClass.info} >
+                        <div className={itemClass.thePhoto}>
+                              <img src={selected.url} alt="image"onClick={funcOpen} />
+                        </div>
+                        <div className={itemClass.description}>
+                              <div className={itemClass.title}>
+                                    {selected.title}
+                              </div>
+                              <div className={itemClass.weight}>
+                                    { selected.weight || selected.weight.length ? `${selected.weight}г` : 'Неизвестно'}
+                              </div>
+                              <div className={itemClass.price}>
+                                    {selected.price}сом
+                              </div>
+                        </div>
+                  </div>
+                  <Amount/>
             </div>
+            {openIsTheSelectedClick ? (<Modal isOpen={funcOpen} infoData={selected}>
+                                    <DescAbout isOpen={funcOpen} desc={selected}/>
+                                    </Modal>):null}
+      </div>
       )
 }
 export default ItemSelected
--- src/components/selected-food/SelectedFood.jsx
+++ src/components/selected-food/SelectedFood.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import Amount from '../amount/Amount';
 import Modal from '../Modal/Modal';
 import DescAbout from '../desc-about-food/DescAbout';
+import ItemSelected from '../item-selected/ItemSelected';
 const SelectedFood = ()=>{
       const inData = useSelector(state=>state.foodsCart.cartStateNow)
       const [openIsTheSelectedClick,setIsOpenTheSelectedClick]=useState(false)
@@ -22,31 +23,8 @@ const SelectedFood = ()=>{
                               </div>
                         ):(
                         inData.map(selected=>(
-                              <div className={selectedFoodClass.content} key={selected.id} >
-                                    <div className={selectedFoodClass.line}>
-                                    </div>                  
-                                    <div className={selectedFoodClass.contentTwo}>
-                                          <div className={selectedFoodClass.info} >
-                                                <div className={selectedFoodClass.thePhoto}>
-                                                      <img src={selected.url} alt="image"onClick={funcOpen} />
-                                                </div>
-                                                <div className={selectedFoodClass.description}>
-                                                      <div className={selectedFoodClass.title}>
-                                                            {selected.title}
-                                                      </div>
-                                                      <div className={selectedFoodClass.weight}>
-                                                            { selected.weight || selected.weight.length ? `${selected.weight}г` : 'Неизвестно'}
-                                                      </div>
-                                                      <div className={selectedFoodClass.price}>
-                                                            {selected.price}сом
-                                                      </div>
-                                                </div>
-                                          </div>
-                                          <Amount/>
-                                    </div>
-                                    {openIsTheSelectedClick ? (<Modal isOpen={funcOpen} infoData={selected}>
-                                                            <DescAbout isOpen={funcOpen} desc={selected}/>
-                                                            </Modal>):null}
+                              <div key={selected.id}>
+                                    <ItemSelected selected={selected}/>
                               </div>
                         )))}
                         
